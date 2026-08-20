c45e81a37a64459e91a3a6bffe9f9849535fa349
ampsedit <dylancohen7@gmail.com>
2026-08-19 21:23:53 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'garage-gym-v6';
+const CACHE = 'garage-gym-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
